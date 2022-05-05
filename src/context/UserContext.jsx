a5f33ab46a6ceb775361fe6d78a6523127b094b3
Clayton Knapp <csjknapp@gmail.com>
2022-05-05 13:13:22 -0700

    wrote login and logout function in UserContext and added to Provider

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -7,9 +7,28 @@ const UserContext = createContext();
 export default function UserProvider({ children }) {
   const [user, setUser] = useState({ email: null });
 
+  // write login and logout functions here
+  function login(email, password) {
+    console.log('check to see if we hit the login function in UserContext', email, password);
+
+    // first hardcode user
+    if (email === 'bob@bob.com' && password === '111111') {
+      // set the user in context and should redirect to page they were trying to go to
+      setUser({ email: 'bob@bob.com' });
+    } else {
+      //set an error message of Invalid credentials
+      throw new Error('Invalid credentials'); // same as error = { message: 'Invalid Credentials' }
+    }
+  };
+
+  function logout() {
+    //simple logout function just sets email to null
+    setUser({ email: null });
+  }
+
   return (
     <UserContext.Provider
-      value={{ user, setUser }}
+      value={{ user, setUser, login, logout }}
     >
       {children}
     </UserContext.Provider>
