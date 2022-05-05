2542bdf8057bf5f7088d93c57c6e9a74c25b25a4
Clayton Knapp <csjknapp@gmail.com>
2022-05-05 11:25:16 -0700

    login form created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Login from './views/Login';
+import EntryList from './views/EntryList';
+
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+
+      <Route path='/login'>
+        <Login />
+      </Route>
+
+      {/* Root path is to guestbook, but protected route
+      Redirects to /login if no user */}
+      <Route path='/'>
+        <EntryList />
+      </Route>
+
+    </Switch>
+  );
 }
---
+++ src/views/Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignInSubmit(e) {
+    e.preventDefault();
+    console.log('email', email);
+    // const user = await signInUser(email, password);
+    // setUser(user);
+  }
+
+  async function handleSignUpClick() {
+    console.log('email', email);
+    // const user = await signUpUser(email, password);
+    // setUser(user);
+  }
+
+
+
+  return (
+    <div>
+      <h3>Sign Up/Sign In</h3>
+      <form action=""
+        onSubmit={handleSignInSubmit}
+      >
+        <label htmlFor="email">Email: 
+          <input
+            required
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='email'
+            // value={}
+          />
+        </label>
+        <label htmlFor="password">Password: 
+          <input
+            required
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='password'
+            // value={ }
+          />
+        </label>
+        <button
+          aria-label="Sign In"
+        >Sign-In</button>
+        <button
+          areia-label="Sign Up"
+          onClick={handleSignUpClick}
+        >Sign-Up</button>
+      </form>
+    </div>
+  )
+};
